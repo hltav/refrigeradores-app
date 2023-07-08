3771103077078627c19b4f06949be3a7d4da44e1
Hudson Lima Tavares <hudsonlimatavares@gmail.com>
2023-07-08 14:05:23 -0300

    Cadatro e Lista de produtos

---
+++ src/view/CadastroRefrigeradores.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h2>Cadastro de Marca e Modelo de Refrigerador</h2>
+      <!-- Adicione seus campos de entrada e lógica de cadastro aqui -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CadastroRefrigeradores',
+  }
+  </script>
+  
---
+++ src/view/ListaRefrigeradores.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h2>Lista de Marca e Modelo de Refrigerador</h2>
+      <!-- Adicione a lógica para exibir as marcas e modelos cadastrados aqui -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ListaRefrigeradores',
+  }
+  </script>
+  
